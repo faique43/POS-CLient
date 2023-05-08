d8067d845077d42fbbfdb50f1c93f1eb2c5c1484
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-05-08 20:38:01 +0500

    login user and admin redux done

--- client/src/store/authSlice/authSlice.js
+++ client/src/store/authSlice/authSlice.js
@@ -1,7 +1,9 @@
 import {
-    createSlice
+    createSlice,
+    createAsyncThunk,
 } from "@reduxjs/toolkit";
 import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const initialState = {
     username: '',
@@ -14,8 +16,54 @@ const authSlice = createSlice({
     initialState,
     extraReducers: (builder) => {
         // authenticate user
+        builder.addCase(loginUser.pending, (state) => {
+
+        })
+        builder.addCase(loginUser.fulfilled, (state, action) => {
+            console.log(action.payload);
+        })
+        builder.addCase(loginUser.rejected, (state, action) => {
+            console.log(action.payload);
+        })
+
+        // authenticate admin
+        builder.addCase(loginAdmin.pending, (state) => {
+
+        })
+        builder.addCase(loginAdmin.fulfilled, (state, action) => {
+            console.log(action.payload);
+        })
+        builder.addCase(loginAdmin.rejected, (state, action) => {
+            console.log(action.payload);
+        })
     }
 })
 
+const loginUser = createAsyncThunk('auth/loginUser', async (userData, {rejectWithValue}) => {
+    try {
+        const response = await axios.post('http://localhost:5000/api/users/auth', userData)
+
+        return response.data;
+    }
+    catch (error) {
+        return rejectWithValue(error.response.data)
+    }
+})
+
+const loginAdmin = createAsyncThunk('auth/loginAdmin', async (adminData, {rejectWithValue}) => {
+    try {
+        const response = await axios.post('http://localhost:5000/api/users/auth', adminData)
+
+        return response.data;
+    }
+    catch (error) {
+        return rejectWithValue(error.response.data)
+    }
+})
+
+export {
+    loginUser,
+    loginAdmin,
+}
 
 export default authSlice;
