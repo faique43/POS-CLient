76c18d4f6c142d682025bb822b2b6faf567acf0c
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-05-09 02:53:43 +0500

    sales data

--- client/src/pages/admin/dashboardContent/Deposits.jsx
+++ client/src/pages/admin/dashboardContent/Deposits.jsx
@@ -3,23 +3,28 @@ import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
 
+import {useSelector} from 'react-redux'
+
 function preventDefault(event) {
     event.preventDefault();
 }
 
 export default function Deposits() {
+    const orders = useSelector(state => state.kitchen.orders)
+
+    const sales = orders.reduce((accumulator, currentValue) => currentValue.totalPrice + accumulator, 1)
     return (
         <React.Fragment>
-            <Title>Recent Deposits</Title>
+            <Title>Total Sales</Title>
             <Typography component="p" variant="h4">
-                $3,024.00
+                Rs {sales}
             </Typography>
             <Typography color="text.secondary" sx={{ flex: 1 }}>
-                on 15 March, 2019
+                {/* on 15 March, 2019 */}
             </Typography>
             <div>
                 <Link color="primary" href="#" onClick={preventDefault}>
-                    View balance
+                    {/* View balance */}
                 </Link>
             </div>
         </React.Fragment>
