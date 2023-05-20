0da56fdbe9bb49e9546009e6f00dba05ee4d010b
faique43 <fkhan.bese20seecs@seecs.edu.pk>
2023-05-21 01:59:13 +0500

    capitalization

--- Server/Routes/ProductRouter.js
+++ Server/Routes/ProductRouter.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-// import all Controllers from productcontroller
-const productController = require("../Controllers/productController");
+// import all Controllers from ProductController
+const ProductController = require("../Controllers/ProductController");
 
 // @route   GET api/products
 // @desc    Get all products
 // @access  Public
-router.get("/", productController.getProducts);
+router.get("/", ProductController.getProducts);
 
 // @route   GET api/products/:id
 // @desc    Get product by ID
 // @access  Public
-router.get("/:id", productController.getProductById);
+router.get("/:id", ProductController.getProductById);
 
 // @route   POST api/products
 // @desc    Create a product
 // @access  Private
-router.post("/", productController.createProduct);
+router.post("/", ProductController.createProduct);
 
 // @route   PUT api/products/:id
 // @desc    Update a product
 // @access  Private
-router.put("/:id", productController.updateProduct);
+router.put("/:id", ProductController.updateProduct);
 
 // @route   DELETE api/products/:id
 // @desc    Delete a product
 // @access  Private
-router.delete("/:id", productController.deleteProduct);
+router.delete("/:id", ProductController.deleteProduct);
 
 // @route   GET api/products/kitchen/:kitchen
 // @desc    Get products by kitchen
 // @access  Public
-router.get("/kitchen/:kitchen", productController.getProductByKitchen);
+router.get("/kitchen/:kitchen", ProductController.getProductByKitchen);
 
 module.exports = router;
