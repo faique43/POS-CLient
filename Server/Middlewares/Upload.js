a075be8c625929c83781a747fbecb88a8adad21c
faique43 <fkhan.bese20seecs@seecs.edu.pk>
2023-06-02 18:17:42 +0500

    package import

--- Server/Middlewares/Upload.js
+++ Server/Middlewares/Upload.js
@@ -6,15 +6,15 @@ require("dotenv").config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "images",
-    allowed_formats: ["jpg", "png", "gif"],
-  },
+    allowed_formats: ["jpg", "png", "gif"]
+  }
 });
 
 const upload = multer({ storage: storage });
