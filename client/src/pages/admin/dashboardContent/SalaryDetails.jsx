46a445c813c834df8130cb0f8bc04aad5cf1a591
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-06-05 21:43:30 +0500

    padi and unpaid salaries done

--- client/src/pages/admin/dashboardContent/SalaryDetails.jsx
+++ client/src/pages/admin/dashboardContent/SalaryDetails.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
 
 export default function SalaryDetails() {
+    const salaries = useSelector(state => state.salaries.salaries);
+    
     return (
         <React.Fragment>
             <Title>Paid Salaries</Title>
             <Typography component="p" variant="h4">
-                {/* {kitchen1Sales.toLocaleString('en-US', { style: 'currency', currency: 'PKR' })} */}
+                {salaries.reduce((total, currentSalary) => {
+                    if(currentSalary.paid) {
+                        return total + currentSalary.amount;
+                    }
+                    return total;
+                }, 0).toLocaleString('en-US', { style: 'currency', currency: 'PKR' })}
             </Typography>
             <Title>Unpaid Salaries</Title>
             <Typography component="p" variant="h4">
-                {/* {kitchen2Sales.toLocaleString('en-US', { style: 'currency', currency: 'PKR' })} */}
+                {salaries.reduce((total, currentSalary) => {
+                    if(!currentSalary.paid) {
+                        return total + currentSalary.amount;
+                    }
+                    return total;
+                }, 0).toLocaleString('en-US', { style: 'currency', currency: 'PKR' })}
             </Typography>
             <Typography color="text.secondary" sx={{ flex: 1 }}>
                 {/* on 15 March, 2019 */}
