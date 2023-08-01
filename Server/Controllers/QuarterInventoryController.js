d621f16778946666869a4d8eb627181ca5b7fa7f
Faique Ahmad Khan <fkhan.bese20seecs@seecs.edu.pk>
2023-08-02 00:35:32 +0500

    Update QuarterInventoryController.js

--- Server/Controllers/QuarterInventoryController.js
+++ Server/Controllers/QuarterInventoryController.js
@@ -3,7 +3,7 @@ const QuarterInventory = require("../Models/QuarterInventory");
 // @route   GET api/quarterinventory
 // @desc    Get all quarter inventory
 // @access  Public
-exports.get_all_quarterinventory("/", async (req, res) => {
+exports.get_all_quarterinventory = async (req, res) => {
   QuarterInventory.find()
     .sort({ date: -1 })
     .then((quarterinventory) => res.json(quarterinventory))
@@ -12,12 +12,12 @@ exports.get_all_quarterinventory("/", async (req, res) => {
         .status(404)
         .json({ noquarterinventoryfound: "No Quarter Inventory found" })
     );
-});
+};
 
 // @route   GET api/quarterinventory/:id
 // @desc    Get quarter inventory by id
 // @access  Public
-exports.get_by_id("/:id", async (req, res) => {
+exports.get_by_id = async (req, res) => {
   QuarterInventory.findById(req.params.id)
     .then((quarterinventory) => res.json(quarterinventory))
     .catch((err) =>
@@ -25,4 +25,4 @@ exports.get_by_id("/:id", async (req, res) => {
         noquarterinventoryfound: "No Quarter Inventory found with that ID"
       })
     );
-});
+};
